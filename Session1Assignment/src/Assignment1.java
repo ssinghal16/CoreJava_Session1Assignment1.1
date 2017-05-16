848fc61d06cbe54fa804f5409dd0c96f8d61d58c
ssinghal16 <soniyasinghal1991@gmail.com>
2017-05-16 23:37:53 +0530

    Session1Assignment for Java

---
+++ Session1Assignment/src/Assignment1.java
@@ -0,0 +1,15 @@
+//Write a program to print the sum of two numbers without using + operator. Other operators have to be used.
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		
+		int a=10,b=1,c;
+		
+		c=a-(-b);
+		
+		System.out.println("Sum of two number is = "+c);
+		}
+	}
+
+
