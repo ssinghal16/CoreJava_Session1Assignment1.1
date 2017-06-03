9119132e7bc60c164ecc7dd2bab797b6328f8054
ssinghal16 <soniyasinghal1991@gmail.com>
2017-06-03 17:19:44 +0530

    Assignment and output

--- Assignment1.1Session1/src/com/acadgild/assignment1/Assignment.java
+++ Assignment1.1Session1/src/com/acadgild/assignment1/Assignment.java
@@ -4,7 +4,7 @@ package com.acadgild.assignment1;
 
 //Class declaration 
 public class Assignment {
-	//Main methid
+	//Main method
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
